b24dfaf55fbbd7bc7feb3b09e0cd49565fa91a5f
IndraPoonam <iamindra.poonam@gmail.com>
2024-03-13 07:32:14 +0530

    gmail clone

--- my-app-clone/src/App.js
+++ my-app-clone/src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import './App.css';
-// import Main from './Component/Main';
 import Main from './Component/Main';
-// import { Routes, Route } from 'react-router-dom';
-// import Contact from './Routerpractice/Contact';
-// import About from './Routerpractice/About';
-// import Home from './Routerpractice/Home';
-// import Navbar from './Routerpractice/Navbar';
+
 
 function App() {
   return (
    <>
-{/* <Navbar/>
-<Routes>
-<Route path="/contact" element={<Contact/>} />
-<Route path="/about" element={<About/>} />
-<Route path="/home" element={<Home/>} />
-</Routes> */}
-
-
 <Main/>
-
-
-
-   {/* <Main/> */}
- 
    </>
   );
 }
